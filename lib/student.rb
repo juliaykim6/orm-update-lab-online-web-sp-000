b4714bb4f4431e4c4d0e7551066d0d14a1749338
Julia Kim <juliaykim6@gmail.com>
2021-05-03 17:51:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,6 +50,9 @@ attr_reader :id
   end
   
   def self.new_from_db
+    new_student = self.new(row[0], row[1], row[2])
+    new_student
+  end
 
   def update
     sql = "UPDATE students SET name = ?, grade = ? WHERE id  = ?"

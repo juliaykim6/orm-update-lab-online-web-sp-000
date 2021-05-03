3cf04ee58ed399f3f87edb2dcfd879a60cdd58aa
Julia Kim <juliaykim6@gmail.com>
2021-05-03 17:48:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -41,5 +41,10 @@ attr_reader :id
     DB[:conn].execute(sql, self.name, self.grade)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
-end
+  end
+
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id  = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end 
 end

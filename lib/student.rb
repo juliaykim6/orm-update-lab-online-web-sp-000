ca55bb6b9bb990a3a1dfb154324944ab631c3113
Julia Kim <juliaykim6@gmail.com>
2021-05-03 17:54:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,7 +57,7 @@ attr_reader :id
   def self.find_by_name(name)
     sql = "SELECT * FROM students WHERE name = ?"
     result = DB[:conn].execute(sql, name)[0]
-    Student.new(result[0], result[1]. rsult[2])
+    Student.new(result[0], result[1]. result[2])
   end
 
   def update

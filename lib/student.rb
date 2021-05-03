c85f343560b3e8aeaeffb8c2e0f9adf99767bbf3
Julia Kim <juliaykim6@gmail.com>
2021-05-03 17:38:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,4 +10,16 @@ attr_reader :id
     @name = name
     @grade = grade 
   end
+  
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    grade INTEGER
+    )
+    SQL
+
+  DB[:conn].execute(sql)
+end
 end

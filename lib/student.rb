5b75c41bb5832ebfc654b30ea33a4285114e5611
Julia Kim <juliaykim6@gmail.com>
2021-05-03 17:42:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,4 +29,12 @@ attr_reader :id
     SQL
     DB[:conn].execute(sql)
   end
+  
+  def save 
+    sql = <<-SQL
+    INSERT INTO students (name, grade)
+    VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+  end
 end
